7110e7016bf5baa1aa7c92ba1a2fd0cef8356da0
Luan Pimenta Fernandes <pimentaluan2005@gmail.com>
2024-01-03 08:29:56 -0300

    Atualização da HOME,cria css do orçamento

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,6 @@ import Logo from '../imgs/Logo Grande.png'
 import DartIcon from '../icons/icons8-dardo.svg';
 import  Luca_pimenta_dg from '..//imgs/Mockup LucaPimenta.png'
 import  Colegio_kairos from '..//imgs/Mockup ColegioKairos.png'
-import  Charming_burger from '..//imgs/Mockup CharmingBurger.png'
 
 import '../style/home.css'
 
@@ -17,7 +16,7 @@ function Home(){
     
     return (
         <div>
-            <header>
+            <header id='home_header'>
                 <img src={Logo} alt='Logo Luan Pimenta DEV' className='logo-img'></img>
                 <ul className='menu'>
                     <li><a href='#sobremim'>SOBRE MIM</a></li>
@@ -105,6 +104,7 @@ function Home(){
                         <p>Todo bom profissional precisa de um site com seu portfólio, não é? Afinal, se não fosse importante você não estaria aqui. Esse site foi criado para o designer gráfico, Luca Pimenta com o intuito de demonstrar o seu trabalho para potenciais clientes.</p>
                         <div className='linguagem_projetos'>
                             <a href='#wordpress_card'><FontAwesomeIcon icon={faWordpress} id='wordpress'/></a>
+                            <a href='#elementor_card'><FontAwesomeIcon icon={faElementor} id='elementor'/></a>
                             <a href='#css_card'><FontAwesomeIcon icon={faCss3Alt} id='css'/></a>
                         </div>
                         <div className='preview_projetos'>
@@ -118,11 +118,53 @@ function Home(){
                         <p>Desenvolvi um site para o Colégio Kairós, uma instituição de ensino em João Pessoa, Brasil. O site fornece informações sobre a escola, galeria de imagens, notícias atualizadas e detalhes de contato. A criação do site foi feita para refletir os valores do Colégio Kairós e seu compromisso com a educação.</p>
                         <div className='linguagem_projetos'>
                             <a href='#wordpress_card'><FontAwesomeIcon icon={faWordpress} id='wordpress'/></a>
+                            <a href='#elementor_card'><FontAwesomeIcon icon={faElementor} id='elementor'/></a>
                             <a href='#css_card'><FontAwesomeIcon icon={faCss3Alt} id='css'/></a>
                         </div>
                         <div className='preview_projetos'>
                             <a href='https://colegiokairosjp.com/' target='blank'><FontAwesomeIcon icon={faPlay} /></a>
                         </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className='orcamento' id='orcamento'>
+                <h1>Orçamento</h1>
+                <div id='orcamento_flex'>
+                    <div className='cards_orcamento'>
+                        <h3>Landing Page</h3>
+                        <h2><span id='moeda'>R$ </span>700 <br />
+                            <span id='manutençao'>+ R$100 de manutenção mensal</span></h2>
+                        <ul>
+                            <li>Página de destino personalizada para atrair clientes</li>
+                            <li>Informações claras sobre produtos/serviços</li>
+                            <li>Formulário de contato de captura de leads</li>
+                            <li>Suporte técnico contínuo</li>
+                        </ul>
+                    </div>
+
+                    <div className='cards_orcamento'>
+                        <h3>Site de Portfólio Pessoal</h3>
+                        <h2><span id='moeda'>R$ </span>1.300 <br />
+                            <span id='manutençao'>+ R$150 de manutenção mensal</span></h2>
+                        <ul>
+                            <li>Apresentação profissional de seu trabalho</li>
+                            <li>Até 5 páginas de portfólio personalizadas</li>
+                            <li>Atualizações regulares de conteúdo</li>
+                            <li>Suporte técnico contínuo</li>
+                        </ul>
+                    </div>
+
+                    <div className='cards_orcamento'>
+                        <h3>Site de Negócios Básico</h3>
+                        <h2><span id='moeda'>R$ </span>3.000 <br />
+                            <span id='manutençao'>+ R$200 de manutenção mensal</span></h2>
+                        <ul>
+                            <li>Presença online completa, até 10 páginas</li>
+                            <li>Informações sobre sua empresa, serviços e equipe</li>
+                            <li>Integração de mídias sociais e informações de contato</li>
+                            <li>Suporte técnico e atualizações regulares</li>
+                        </ul>
                     </div>
                 </div>
             </section>
